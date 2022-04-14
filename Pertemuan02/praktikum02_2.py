8df7716292c81f23df638e1237477c65a46cadba
mfakhrulam <aminfakhrul99@gmail.com>
2022-04-14 09:49:44 +0700

    memodifikasi file praktikum02_2.py dan praktikum02_3.py

--- Pertemuan02/praktikum02_2.py
+++ Pertemuan02/praktikum02_2.py
@@ -5,25 +5,38 @@ from kanren.goals import membero
 
 suka = Relation()
 facts(suka, ("ellen", "tenis"),
-            ("john", "football"),
+            ("ellen", "renang"),    # tambah fakta
+            ("john", "sepakbola"),
             ("mary", "renang"),
+            ("mary", "basket"),     # tambah fakta
             ("tom", "tenis"),
-            ("eric", "renang"))
-
+            ("tom", "basket"),      # tambah fakta
+            ("eric", "renang"),
+            ("ann", "sepakbola"),   # tambah fakta
+            ("ann", "basket"))      # tambah fakta
+# listing program 2.2
 x = var()
 tom_hobbies = run(0, x, suka("tom", x))
-print("Tom: ", tom_hobbies)
-
 for hobby in tom_hobbies:
     fact(suka, ("bill"), hobby)
 bill_hobbies = run(0, x, suka("bill", x))
-print("Bill: ", bill_hobbies)
-
 mary_hobbies = run(0, x, suka("mary", x))
-print("Mary: ", mary_hobbies)
-
 for hobby in mary_hobbies:
     fact(suka, ("ann"), hobby)
 ann_hobbies = run(0, x, suka("ann", x))
-print("Ann: ", ann_hobbies)
 
+# Query 1
+ellen_hobbies = run(0, x, suka("ellen", x))
+tom_hobbies = run(0, x, suka("tom", x))
+print("ellen:", ellen_hobbies)
+print("Tom:", tom_hobbies)
+q1 = run(0, x, membero(x, ellen_hobbies), membero(x, tom_hobbies))
+print("Hobi Ellen dan Tom yang sama :", q1)
+
+# Query 2
+mary_hobbies = run(0, x, suka("mary", x))
+ann_hobbies = run(0, x, suka("ann", x))
+print("\nmary:", mary_hobbies)
+print("Ann: ", ann_hobbies)
+q2 = run(0, x, membero(x, mary_hobbies), membero(x, ann_hobbies))
+print("Hobi Mary dan Ann yang sama :", q2)
--- Pertemuan02/praktikum02_3.py
+++ Pertemuan02/praktikum02_3.py
@@ -6,6 +6,7 @@ from kanren import vars
 ukuran = Relation()
 warna = Relation()
 gelap = Relation()
+jenis = Relation()
 
 facts(ukuran, ("beruang", "besar"),
               ("gajah", "besar"),
@@ -13,10 +14,40 @@ facts(ukuran, ("beruang", "besar"),
 facts(warna, ("beruang", "cokelat"),
              ("kucing", "hitam"),
              ("gajah", "kelabu"))
+facts(jenis, ("beruang", "karnivora"),
+             ("kucing", "karnivora"))
 
 fact(gelap, "hitam")
 fact(gelap, "cokelat")
-
+ 
 x = var()
+
 kecil = run(0, x, ukuran(x, "kecil"))
 print("hewan berukuran kecil: ", kecil)
+
+besar = run(0, x, ukuran(x, "besar"))
+print("hewan berukuran besar: ",besar)
+
+cokelat = run(0, x, warna(x, "cokelat"))
+print("hewan berwarna cokelat: ", cokelat)
+
+besar_cokelat = run(0, x, membero(x, besar), membero(x, cokelat))
+print("hewan berukuran besar dan berwarna cokelat: ", besar_cokelat)
+
+warna_gelap = run(0, x, gelap(x))
+print("warna gelap: ", warna_gelap)
+hewan_gelap = []
+for i in warna_gelap:
+    temp = run(0, x, warna(x, i))
+    temp = [x for x in temp]
+    hewan_gelap += temp
+print("hewan berwarna gelap: ", tuple(hewan_gelap))
+
+hewan_gelap_dan_besar = []
+for i in warna_gelap:
+    temp = run(0, x, warna(x, i), ukuran(x, "besar"))
+    temp = [x for x in temp]
+    hewan_gelap_dan_besar += temp
+print("hewan berukuran besar dan berwarna gelap: ", tuple(hewan_gelap_dan_besar))
+hewan_karnivora = run(0, x, jenis(x, "karnivora"))
+print("hewan karnivora: ", hewan_karnivora)
